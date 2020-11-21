89d9ae224434e879c839e4c83de6bc30dda6570d
unknown <maksdev01panel@gmail.com>
2020-11-21 21:21:53 +0200

    Updates

--- src/components/api/api.js
+++
@@ -1,9 +0,0 @@
-import json from './news.json'
-
-let url = json
-
-export const getNews = async () =>{
-    const response =  await fetch(url)
-    const data = await response.json()
-    return data
-}
